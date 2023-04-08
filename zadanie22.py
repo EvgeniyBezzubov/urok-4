468be38066ed0ff36198c9cf544e2bf02878b65c
Evgeniy.Bezzubov <raindrops228@gmail.com>
2023-04-08 21:27:43 +1000

    2 задания готово

---
+++ zadanie22.py
@@ -0,0 +1,17 @@
+def massive_bezpovtorov():
+    m = int(input("кол-во элементов первого множестава:"))
+    n = int(input("кол-во элементов второго множестава:"))
+    lst_m = []
+    lst_n = []
+    for i in range(m):
+        lst_m.append(int(input("введите элемент {0} первого множества : ".format(i))))
+    for i in range(n):
+        lst_n.append(int(input("введите элемент {0} второго множества : ".format(i))))
+
+    lst_m_1 = list(set(lst_m))
+    lst_n_1 = list(set(lst_n))
+    lst_m_1.sort()
+    lst_n_1.sort()
+    print(lst_m_1)
+    print(lst_n_1)
+massive_bezpovtorov()
